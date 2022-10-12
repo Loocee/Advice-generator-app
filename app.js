fc9efa9d0c2b95e8a2cf947959d1da93058005c1
Loocee <favournkemdirim7@gmail.com>
2022-10-12 14:26:58 +0100

    update index.html and add animation in app.js

--- app.js
+++ app.js
@@ -4,15 +4,26 @@ $(document).ready(function () {
   const advice = $("#advice");
   const dice = $(".dice");
   const url = "https://api.adviceslip.com/advice";
+  const element = $(".dice-img");
 
+  var tmpAnimation = 0;
 
   dice.on("click", function () {
+    tmpAnimation = tmpAnimation + 360;
+
+    $({degrees: tmpAnimation - 360}).animate({degrees: tmpAnimation}, {
+        duration: 1000,
+        step: function(now) {
+            element.css({
+                transform: 'rotate(' + now + 'deg)'
+            });
+        }
+    });
 
     fetch(url, {cache: 'no-cache'}) // fetching data from the api // *default, no-cache, reload, force-cache, only-if-cached
   .then((response) => response.json())
   .then((response) => {
     let data = response.slip;
-    console.log(data);
     head.html("Advice #" + data.id);
     advice.html(data.advice);
   });
